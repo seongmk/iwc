3ae14a57ddcd8ff46d5d6e33e48c6021c7685242
seongmk <64449248+seongmk@users.noreply.github.com>
2020-05-18 17:55:47 +0900

    Add files via upload

---
+++ js/main_banner.js
@@ -0,0 +1,12 @@
+$(function(){
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  })
+})
